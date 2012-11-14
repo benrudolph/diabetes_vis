81065bdb43a547632ca20d71c3544962c02b7fa4
Ben Rudolph <rudolphben@gmail.com>
2012-11-13 22:11:40 -0800

    Rails code added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiabetesVis::Application.config.secret_token = '0d18e8b6160252d750080ad196e1eafb36ea1e8d56756377fda5ea394bd1796ccc68750fe23159624fa974c0aa8d2c3348945adcfc635f010a3190b6e50a5311'
