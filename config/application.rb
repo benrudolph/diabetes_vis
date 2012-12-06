9e1aa958f3eae7106f4f140c57659a2d516d665e
Ben Rudolph <rudolphben@gmail.com>
2012-12-05 19:09:14 -0800

    Remove line in config

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,6 @@ module DiabetesVis
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    config.assets.initialize_on_precompile = false
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
